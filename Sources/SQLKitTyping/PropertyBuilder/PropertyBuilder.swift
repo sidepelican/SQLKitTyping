11e3aaa9748968412cc213eb438bc9283bc27e43
Iceman <okamura@qoncept.co.jp>
2024-06-12 15:50:37 +0900

    numbering group

--- Sources/SQLKitTyping/PropertyBuilder/PropertyBuilder.swift
+++ Sources/SQLKitTyping/PropertyBuilder/PropertyBuilder.swift
@@ -143,16 +143,55 @@ public struct PropertyBuilder {
     }
 }
 
-public func group<T>(@PropertyBuilder _ build: () -> PropertyBuilder.Result<T>) -> some PropertySQLExpression<GroupedProperty<T>> {
-    return GroupExpression<GroupedProperty<T>>(expressions: build().columns)
+public func group1<T>(@PropertyBuilder _ build: () -> PropertyBuilder.Result<T>) -> some PropertySQLExpression<GroupedProperty1<T>> {
+    return GroupExpression<_>(expressions: build().columns)
 }
 
-public struct GroupedProperty<Intersection: Decodable>: Decodable {
-    public var group: Intersection
+public func group2<T>(@PropertyBuilder _ build: () -> PropertyBuilder.Result<T>) -> some PropertySQLExpression<GroupedProperty2<T>> {
+    return GroupExpression<_>(expressions: build().columns)
+}
+
+public func group3<T>(@PropertyBuilder _ build: () -> PropertyBuilder.Result<T>) -> some PropertySQLExpression<GroupedProperty3<T>> {
+    return GroupExpression<_>(expressions: build().columns)
+}
+
+public func group4<T>(@PropertyBuilder _ build: () -> PropertyBuilder.Result<T>) -> some PropertySQLExpression<GroupedProperty4<T>> {
+    return GroupExpression<_>(expressions: build().columns)
+}
+
+public struct GroupedProperty1<Intersection: Decodable>: Decodable {
+    public var group1: Intersection
 
     public init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
-        group = try container.decode(Intersection.self)
+        group1 = try container.decode(Intersection.self)
+    }
+}
+
+public struct GroupedProperty2<Intersection: Decodable>: Decodable {
+    public var group2: Intersection
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        group2 = try container.decode(Intersection.self)
+    }
+}
+
+public struct GroupedProperty3<Intersection: Decodable>: Decodable {
+    public var group3: Intersection
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        group3 = try container.decode(Intersection.self)
+    }
+}
+
+public struct GroupedProperty4<Intersection: Decodable>: Decodable {
+    public var group4: Intersection
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        group4 = try container.decode(Intersection.self)
     }
 }
 
