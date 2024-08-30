508a6ad5d2bcfdf8f4e33ad504498d86ec79a26a
kenta okamura <side.junktown@gmail.com>
2024-08-30 14:48:40 +0900

    Add SQLQueryString overload

--- Sources/SQLKitTypingMacros/Macros/SQLColumnPropertyType.swift
+++ Sources/SQLKitTypingMacros/Macros/SQLColumnPropertyType.swift
@@ -38,6 +38,9 @@ struct \(raw: arguments.name)<Value: Decodable>: PropertySQLExpression {
     init(_ expr: any SQLExpression, as type: Value.Type) {
         self.expr = expr
     }
+    init(_ queryString: SQLQueryString, as type: Value.Type) {
+        self.expr = queryString
+    }
     var expr: any SQLExpression
     struct Property: Decodable {
         var \(raw: arguments.name): Value
